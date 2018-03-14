33836dce6d75febe216206a9efa18bf183e49672
Stephanie Chen <stephanie.chen@thomsonreuters.com>
2018-03-14 13:03:00 -0400

    simple_linear_regression

--- simple-linear-regression/simple_linear_regression.py
+++ simple-linear-regression/simple_linear_regression.py
@@ -7,13 +7,13 @@ import pandas as pd
 import os
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
 #Feature Scaling
 """from sklearn.preprocessing import StandardScaler
